6065b094ec9fe80d4eb69fb34dc85e0030ef78cb
PRADEEPDECE <PRADEEP.DECE@MSN.COM>
2021-02-07 16:09:47 +0530

    DAY 1  Assignment Submitted

---
+++ DSA PYTHON/DAY 1/Python Program To Addition Subtraction Multiplication Division.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 15:41:27 2021
+
+@author: LENOVO
+"""
+
+import math
+
+X=int(input("Enter the value of X: "))
+Y=int(input("Enter the value of Y: "))
+res = math.pow(X,Y)
+print("\n X^Y Result = ", res)
+
+     
